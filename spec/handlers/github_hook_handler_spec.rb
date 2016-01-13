b1f58dbfd299558d9fc4c27f0dd43d143ed387a3
Cameron Dutro <camertron@gmail.com>
2016-01-12 16:40:58 -0800

    Got a test working for the github hook

---
+++ spec/handlers/github_hook_handler_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+require 'helpers/github_payload_builder'
+require 'helpers/nil_logger'
+
+include Txgh
+include Txgh::Handlers
+
+describe GithubHookHandler do
+  include StandardTxghSetup
+
+  let(:handler) do
+    GithubHookHandler.new(
+      project: transifex_project,
+      repo: github_repo,
+      payload: payload.to_h,
+      logger: logger
+    )
+  end
+
+  let(:logger) do
+    NilLogger.new
+  end
+
+  let(:payload) do
+    GithubPayloadBuilder.webhook_payload(repo_name, branch)
+  end
+
+  let(:modified_files) do
+    file_sha = 'def456'
+    transifex_project.resources.map do |resource|
+      { 'path' => resource.source_file, 'sha' => file_sha.next! }
+    end
+  end
+
+  it 'correctly uploads modified files to transifex' do
+    tree_sha = 'abc123'
+
+    # indicate that all the files we care about have changed
+    payload.add_commit(
+      modified: modified_files.map { |f| f['path'] }
+    )
+
+    expect(github_api).to(
+      receive(:get_commit).with(repo_name, payload.commits.first[:id]) do
+        { 'commit' => { 'tree' => { 'sha' => tree_sha } } }
+      end
+    )
+
+    expect(github_api).to(
+      receive(:tree).with(repo_name, tree_sha) do
+        { 'tree' => modified_files }
+      end
+    )
+
+    modified_files.each do |file|
+      translations = "translations for #{file['path']}"
+
+      expect(github_api).to(
+        receive(:blob).with(repo_name, file['sha']) do
+          { 'content' => translations, 'encoding' => 'utf-8' }
+        end
+      )
+
+      expect(transifex_api).to(
+        receive(:update) do |resource, content|
+          expect(resource.source_file).to eq(file['path'])
+          expect(content).to eq(translations)
+        end
+      )
+    end
+
+    handler.execute
+  end
+end
