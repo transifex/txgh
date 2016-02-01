62a38a5e455799c25627fce5623740693d68e138
Cameron Dutro <camertron@gmail.com>
2016-02-01 15:30:39 -0800

    Shore up KeyManager specs

--- spec/key_manager_spec.rb
+++ spec/key_manager_spec.rb
@@ -16,14 +16,6 @@ describe KeyManager do
       expect(config.project_config).to eq(project_config)
       expect(config.repo_config).to eq(repo_config)
     end
-
-    # it 'loads tx config from the given file if not explicitly passed in' do
-    #   path = 'path/to/tx_config'
-    #   project_config.merge!('tx_config' => path)
-    #   expect(TxConfig).to receive(:load_file).with(path).and_return(:tx_config)
-    #   config = KeyManager.config_from_project(project_name)
-    #   expect(config.tx_config).to eq(:tx_config)
-    # end
   end
 
   describe '.config_from_repo' do
@@ -37,14 +29,6 @@ describe KeyManager do
       expect(config.project_config).to eq(project_config)
       expect(config.repo_config).to eq(repo_config)
     end
-
-    # it 'loads tx config from the given file if not explicitly passed in' do
-    #   path = 'path/to/tx_config'
-    #   project_config.merge!('tx_config' => path)
-    #   expect(TxConfig).to receive(:load_file).with(path).and_return(:tx_config)
-    #   config = KeyManager.config_from_repo(repo_name)
-    #   expect(config.tx_config).to eq(:tx_config)
-    # end
   end
 
   describe '.config_from' do
@@ -58,13 +42,43 @@ describe KeyManager do
       expect(config.project_config).to eq(project_config)
       expect(config.repo_config).to eq(repo_config)
     end
+  end
 
-    # it 'loads tx config from the given file if not explicitly passed in' do
-    #   path = 'path/to/tx_config'
-    #   project_config.merge!('tx_config' => path)
-    #   expect(TxConfig).to receive(:load_file).with(path).and_return(:tx_config)
-    #   config = KeyManager.config_from(project_name, repo_name)
-    #   expect(config.tx_config).to eq(:tx_config)
-    # end
+  describe '.tx_config' do
+    let(:config) { KeyManager.config_from(project_name, repo_name) }
+    let(:project) { config.transifex_project }
+    let(:repo) { config.github_repo }
+
+    it 'loads tx config from the given file' do
+      path = 'file://path/to/tx_config'
+      project_config.merge!('tx_config' => path)
+      expect(TxConfig).to receive(:load_file).with('path/to/tx_config').and_return(:tx_config)
+      config = KeyManager.tx_config(project, repo)
+      expect(config).to eq(:tx_config)
+    end
+
+    it 'raises an error if asked to load config from a git repository and no ref is given' do
+      project_config.merge!('tx_config' => 'git://./tx.config')
+      expect { KeyManager.tx_config(project, repo) }.to raise_error(TxghError)
+    end
+
+    it 'loads tx config from a git repository' do
+      project_config.merge!('tx_config' => 'git://./tx.config')
+
+      expect(repo.api).to(
+        receive(:download)
+          .with('./tx.config', 'my_branch')
+          .and_return("[main]\nlang_map = ko:ko_KR")
+      )
+
+      config = KeyManager.tx_config(project, repo, 'my_branch')
+      expect(config.lang_map).to eq({ 'ko' => 'ko_KR' })
+    end
+
+    it 'loads raw tx config' do
+      project_config.merge!('tx_config' => "raw://[main]\nlang_map = ko:ko_KR")
+      config = KeyManager.tx_config(project, repo)
+      expect(config.lang_map).to eq({ 'ko' => 'ko_KR' })
+    end
   end
 end
