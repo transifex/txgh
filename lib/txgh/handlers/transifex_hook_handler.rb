7afb73ecfe3723e0000cf9af3f8f93b89fe5a97c
Cameron Dutro <camertron@gmail.com>
2016-02-05 10:48:12 -0800

    First pass at pulling

--- lib/txgh/handlers/transifex_hook_handler.rb
+++ lib/txgh/handlers/transifex_hook_handler.rb
@@ -19,25 +19,10 @@ module Txgh
         logger.info(resource_slug)
 
         if tx_resource
-        # Do not update the source
           unless language == tx_resource.source_lang
             logger.info('request language matches resource')
-
-            translations = project.api.download(tx_resource, language)
-
-            translation_path = if tx_resource.lang_map(language) != language
-              logger.info('request language is in lang_map and is not in request')
-              tx_resource.translation_path(tx_resource.lang_map(language))
-            else
-              logger.info('request language is in lang_map and is in request or is nil')
-              tx_resource.translation_path(tx_resource.lang_map(language))
-            end
-
-            logger.info("make github commit for branch: #{branch}")
-
-            repo.api.commit(
-              repo.name, branch, translation_path, translations
-            )
+            committer = ResourceCommitter.new(project, repo, logger)
+            committer.commit_resource(tx_resource, branch, language)
           end
         else
           raise TxghError,
