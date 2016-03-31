b16938cb991700bf4d2f1d6636d73e635609b4b4
Cameron Dutro <camertron@gmail.com>
2016-03-31 12:28:25 -0700

    Place shared stream concerns into base class

--- lib/txgh/handlers.rb
+++ lib/txgh/handlers.rb
@@ -2,6 +2,7 @@ module Txgh
   module Handlers
     autoload :Github,            'txgh/handlers/github'
     autoload :Response,          'txgh/handlers/response'
+    autoload :StreamResponse,    'txgh/handlers/stream_response'
     autoload :TgzStreamResponse, 'txgh/handlers/tgz_stream_response'
     autoload :Transifex,         'txgh/handlers/transifex'
     autoload :Triggers,          'txgh/handlers/triggers'
