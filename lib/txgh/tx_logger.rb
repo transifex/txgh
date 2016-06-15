4e82e67f1e6846eae6cd46c0b36cfe66083cdcba
anero <anero79@gmail.com>
2016-06-15 11:43:13 -0300

    Log to file

--- lib/txgh/tx_logger.rb
+++ lib/txgh/tx_logger.rb
@@ -3,8 +3,9 @@ require 'logger'
 module Txgh
   class TxLogger
     def self.logger
-      @_logger ||= Logger.new(STDOUT).tap do |logger|
-        logger.level = Logger::INFO
+      log_file_path = Pathname(File::dirname(__FILE__)).join("../../log/#{ENV['RACK_ENV']}.log")
+      @_logger ||= Logger.new(log_file_path).tap do |logger|
+        logger.level = Logger::DEBUG
         logger.datetime_format = '%a %d-%m-%Y %H%M '
       end
     end
