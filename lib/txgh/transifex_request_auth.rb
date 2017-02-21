a14cc06066d682827fc3bf6a4dc6e3d22328aa0a
Sofia Margariti <sofia@transifex.com>
2017-02-21 15:51:04 +0200

    Add tests for v2 signature calculation

--- lib/txgh/transifex_request_auth.rb
+++ lib/txgh/transifex_request_auth.rb
@@ -10,31 +10,29 @@ module Txgh
 
     class << self
       def authentic_request?(request, secret)
-        expected_signature_v1 = header_value_v1(request, secret)
-        expected_signature_v2 = header_value_v2(request, secret)
-        actual_signature_v1 = request.env[RACK_HEADER]
-        actual_signature_v2 = request.env[RACK_HEADER_V2]
-        actual_signature_v1 == expected_signature_v1 or actual_signature_v2 == expected_signature_v2
-      end
-
-      def header_value_v1(request, secret)
-        request.body.rewind
-        content = request.body.read
-        if request.env['CONTENT_TYPE'] == "application/json"
-          params = JSON.parse(content)
-        else
-          params = URI.decode_www_form(content)
-        end
-        digest(HMAC_DIGEST, secret, transform(params))
-      end
-
-      def header_value_v2(request, secret)
         request.body.rewind
         content = request.body.read
         http_verb = request.request_method
         url = request.url
         date = request.env['HTTP_DATE']
-        content_md5 = request.env['HTTP_CONTENT_MD5']
+        if request.env['CONTENT_TYPE'] == "application/json"
+          params = JSON.parse(content)
+        else
+          params = URI.decode_www_form(content)
+        end
+        expected_signature_v1 = header_value_v1(params, secret)
+        expected_signature_v2 = header_value_v2(http_verb, url, date, content, secret)
+        actual_signature_v1 = request.env[RACK_HEADER]
+        actual_signature_v2 = request.env[RACK_HEADER_V2]
+        actual_signature_v1 == expected_signature_v1 or actual_signature_v2 == expected_signature_v2
+      end
+
+      def header_value_v1(params, is_json)
+        digest(HMAC_DIGEST, secret, transform(params))
+      end
+
+      def header_value_v2(http_verb, url, date, content, secret)
+        content_md5 = Digest::MD5.hexdigest content
         data = [http_verb, url, date, content_md5].join("\n")
         digest(HMAC_DIGEST_256, secret, data)
       end
