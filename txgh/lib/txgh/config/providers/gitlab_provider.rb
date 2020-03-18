fe56774b1ee98403abdf89923ea1db7f6214e07e
Ivan Danci <bah4uk@gmail.com>
2020-03-18 18:11:01 +0200

    Inherit GitlabProvider

--- txgh/lib/txgh/config/providers/gitlab_provider.rb
+++ txgh/lib/txgh/config/providers/gitlab_provider.rb
@@ -1,25 +1,7 @@
 module Txgh
   module Config
     module Providers
-      class GitlabProvider
-        SCHEME = 'git'
-
-        class << self
-          def supports?(scheme)
-            scheme == SCHEME
-          end
-
-          def load(payload, parser, options = {})
-            new(payload, parser, options).config
-          end
-
-          def scheme
-            SCHEME
-          end
-        end
-
-        attr_reader :payload, :parser
-
+      class GitlabProvider < Txgh::Config::Providers::GitlabProvider
         def initialize(payload, parser, options = {})
           @payload = payload
           @parser = parser
@@ -27,25 +9,12 @@ module Txgh
           @gitlab_repo = options[:gitlab_repo]
         end
 
-        def config
-          parser.load(download)
-        end
-
         private
 
         def download
           gitlab_repo.api.download(payload, ref)[:content]
-        rescue Octokit::NotFound
+        rescue Gitlab::Error::NotFound
           raise Txgh::GitConfigNotFoundError, "Config file #{payload} not found in #{ref}"
-        end
-
-        def ref
-          unless @ref
-            raise TxghError,
-              "TX_CONFIG specified a file from git but did not provide a ref."
-          end
-
-          @ref
         end
 
         def gitlab_repo
