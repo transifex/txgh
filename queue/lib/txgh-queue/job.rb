0b5d47e1056004fe1b546a69e240a28f7e47f29d
Ivan Danci <bah4uk@gmail.com>
2020-03-20 22:37:48 +0200

    Fix gitlab hook name

--- queue/lib/txgh-queue/job.rb
+++ queue/lib/txgh-queue/job.rb
@@ -39,8 +39,6 @@ module TxghQueue
           handle_github_delete(project, repo, payload)
         when 'gitlab.push'
           handle_gitlab_push(project, repo, payload)
-        when 'gitlab.delete'
-          handle_gitlab_delete(project, repo, payload)
         when 'transifex.hook'
           handle_transifex_hook(project, repo, payload)
       end
@@ -70,12 +68,6 @@ module TxghQueue
     def handle_gitlab_push(project, repo, payload)
       attributes = TxghServer::Webhooks::Gitlab::PushAttributes.new(payload)
       handler = TxghServer::Webhooks::Gitlab::PushHandler.new(project, repo, logger, attributes)
-      execute(handler)
-    end
-
-    def handle_gitlab_delete(project, repo, payload)
-      attributes = TxghServer::Webhooks::Gitlab::DeleteAttributes.new(payload)
-      handler = TxghServer::Webhooks::Gitlab::DeleteHandler.new(project, repo, logger, attributes)
       execute(handler)
     end
 
--- queue/lib/txgh-queue/webhooks/gitlab/request_handler.rb
+++ queue/lib/txgh-queue/webhooks/gitlab/request_handler.rb
@@ -7,26 +7,18 @@ module TxghQueue
         def handle_request
           handle_safely do
             case gitlab_event
-              when 'push', 'delete'
-                txgh_event = "gitlab.#{gitlab_event}"
+              when 'Push Hook'
+                txgh_event = "gitlab.push"
 
                 result = ::TxghQueue::Config.backend
                   .producer_for(txgh_event, logger)
                   .enqueue(attributes.to_h.merge(txgh_event: txgh_event))
 
                 respond_with(202, result.to_json)
-              when 'ping'
-                ping_handler.execute
               else
                 respond_with_error(400, "Event '#{gitlab_event}' cannot be enqueued")
             end
           end
-        end
-
-        private
-
-        def ping_handler
-          @ping_handler ||= TxghServer::Webhooks::Gitlab::PingHandler.new(logger)
         end
       end
     end
