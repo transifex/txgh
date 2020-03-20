083903b4afe0a7f99f28a324608c7549a1cdee6c
Ivan Danci <bah4uk@gmail.com>
2020-03-20 16:43:58 +0200

    Fix gitlab status updater

--- server/lib/txgh-server/webhooks/gitlab/status_updater.rb
+++ server/lib/txgh-server/webhooks/gitlab/status_updater.rb
@@ -5,31 +5,28 @@ module TxghServer
     module Gitlab
       class StatusUpdater < TxghServer::Webhooks::Github::StatusUpdater
         def report_error_and_update_status(error)
-          # TODO:
           error_params = Txgh.events.publish_error!(error)
 
-          Txgh.events.publish_each('github.status.error', error_params) do |status_params|
+          Txgh.events.publish_each('gitlab.status.error', error_params) do |status_params|
             if status_params
               update_status_safely do
-                Txgh::GithubStatus.error(project, repo, branch, status_params)
+                Txgh::GitlabStatus.error(project, repo, branch, status_params)
               end
             end
           end
         end
 
         def update_status
-          # TODO:
           update_status_safely do
-            Txgh::GithubStatus.update(project, repo, branch)
+            Txgh::GitlabStatus.update(project, repo, branch)
           end
         end
 
         private
 
         def update_status_safely
-          # TODO:
           yield
-        rescue Octokit::UnprocessableEntity
+        rescue ::Gitlab::Error::Unprocessable
           # raised because we've tried to create too many statuses for the commit
         rescue Txgh::TransifexNotFoundError
           # raised if transifex resource can't be found
