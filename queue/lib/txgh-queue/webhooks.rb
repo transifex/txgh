88d7ab2b71423969f280ce7501b825da84b02be2
Ivan Danci <bah4uk@gmail.com>
2020-03-23 23:57:02 +0200

    Fix queue autoload

--- queue/lib/txgh-queue/webhooks.rb
+++ queue/lib/txgh-queue/webhooks.rb
@@ -1,6 +1,7 @@
 module TxghQueue
   module Webhooks
     autoload :Github,    'txgh-queue/webhooks/github'
+    autoload :Gitlab,    'txgh-queue/webhooks/gitlab'
     autoload :Transifex, 'txgh-queue/webhooks/transifex'
   end
 end
---
+++ queue/lib/txgh-queue/webhooks/gitlab.rb
@@ -0,0 +1,7 @@
+module TxghQueue
+  module Webhooks
+    module Gitlab
+      autoload :RequestHandler, 'txgh-queue/webhooks/gitlab/request_handler'
+    end
+  end
+end
